fbc01401347a481324195d5ce6ffe894bed4b5d7
mw7813 <mw7813@naver.com>
2025-02-10 15:31:56 +0900

    feat:작성파일 추가

---
+++ SpringProjectForJenkins/src/main/java/com/min/edu/HomeController.java
@@ -0,0 +1,16 @@
+package com.min.edu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public String home(Model model) {
+		model.addAttribute("result","안녕하세요 CI/CD교육중입니다");
+		
+		return "home";
+	}
+}
